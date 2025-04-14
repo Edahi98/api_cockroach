2d9a5ce12f2cd4be884fb8fb08270461d6c65731
Edahi98 <garciaedahi3822@gmail.com>
2025-04-13 23:12:16 -0600

    update sys 10

--- src/flask_server/resolves_queries.py
+++ src/flask_server/resolves_queries.py
@@ -8,6 +8,7 @@ def login(_, info, pwd, nickname):
         print(f"LOGIN: PWD {pwd}")
         print(f"LOGIN: NICK {nickname}")
         resultado = UserModel.select().where(UserModel.nickname == nickname).get()
+        print(f"RESULT BD: PWD {resultado.password}")
         if HasherPWD.check(resultado.password, pwd):
             return {
                 "token": AuthJWT.generate_token(nickname),
