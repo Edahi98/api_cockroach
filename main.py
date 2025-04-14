935df53f823128fc7731882aee542fbe45a12793
Edahi98 <garciaedahi3822@gmail.com>
2025-04-13 19:50:06 -0600

    update sys 7

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from src.flask_server.context_db import run_db
 
 if __name__ == '__main__':
     run_db()
-    app.run(host="0.0.0.0", port=environ.get("PORT"))
+    app.run(host="0.0.0.0", port=10000)
