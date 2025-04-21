44f47084f559357955e85a600a858cc6a431ffcd
Edahi98 <garciaedahi3822@gmail.com>
2025-04-20 21:27:58 -0600

    Se reconstruyo

--- src/flask_server/server.py
+++ src/flask_server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .api_flask import api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,  resources={"*": {"origins": "*"}})
 app.register_blueprint(api)
 
 
